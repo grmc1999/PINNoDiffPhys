4cdfb4a2e7a7734d0b47356318f60cbdcefec6aa
guillermo <gmorenoc@uni.pe>
2026-03-25 01:19:58 -0300

    clear

--- trainer/Trainer.py
+++ trainer/Trainer.py
@@ -97,7 +97,7 @@ class FiredrakeTimeStepper(ABC):
         u_np1 = fd.Function(self.V, name="u_np1_state")
 
         if isinstance(self.point_evaluator, np.ndarray):
-            u_np1 = fd.assemble(fd.interpolate(u_n, self.V))
+            u_np1 = fd.assemble(fd.project(u_n, self.V))
             #u_np1 = fd.project(u_np1,self.V)
         F = self.residual(u_np1, u_n)
         fd.solve(
